3fa0dfdb1fcca4c6d0bf9a604a9675b85e4cfb9d
Lily <ppuntharee28@gmail.com>
2025-05-27 11:20:49 +0700

    learn condition

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,7 +1,17 @@
 export default function AppWelcome(){
+    const title = 'Welcome to cosci'
+    const currentYear = <p>2025</p>
+    const isShow = true;
     return(
         <>
-        Welcome to cosci
+        <p>{title.toUpperCase()}</p>
+        {currentYear}
+        {
+            isShow && <p>Date: 10/10/1998</p>
+        }
+        {
+            isShow? <p>Hello Next.js</p> : <p>Hello JavaScript</p>
+        }
         </>
     );
 }
