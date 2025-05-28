9b762f7ac080e8ac0ccf49c9cc37b7b98d10ec0f
Lily <ppuntharee28@gmail.com>
2025-05-28 10:35:42 +0700

    add template footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Features01Page from "@/components/features-01/features-01";
+import Footer05Page from "@/components/footer-05/footer-05";
 import Hero02 from "@/components/hero-02/hero-02";
 //<div></div> or <> </> it's was the same
 export default function Home() {
   return (
     <> 
       <Hero02/>
+      <Features01Page/>
+      <Footer05Page/>
     </>
   );
 }
